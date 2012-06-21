34064a866a885585845c95cdf7228f794431bd7c
Ni Jiamu <nijiamu@hotmail.com>
2012-06-21 20:16:36 +0300

    Transcript done

--- course.c
+++ course.c
@@ -1,28 +1,67 @@
 #include "course.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+// This is a header file for the Course ADT, which is used to represent rows in
+// a transcript of completed courses. One instance of the ADT stores the name of the course,
+// and the grade received.
+//
+//
+// Implement the struct in your .c file. The field used to store
+// the name of the course must have type char * - you are not allowed to use
+// a static array.
+
+struct Course_s {
+	char* name;
+	int grade;
+};
+
+char* stringCopy(const char* src) {
+	assert(src);
+	char* dest = calloc(strlen(src) + 1, sizeof(char));
+	strcpy(dest, src);
+	return dest;
+}
 
 // Creates a new course with name "courseName" and grade "grade". If courseName
 // is NULL or if the grade is negative or zero (<= 0), the function returns NULL.
-Course *courseConstruct(const char *courseName, int grade) {
-	return NULL;
+Course* courseConstruct(const char* courseName, int grade) {
+	if (courseName == NULL || grade <= 0) {
+		return NULL;
+	}
+	Course* c = calloc(1, sizeof(Course));
+	c->name = stringCopy(courseName);
+	c->grade = grade;
+	return c;
 }
 
 // Frees all memory allocated to the Course in the constructor functions Construct and Copy.
-void courseDestruct(Course * course) {
+void courseDestruct(Course* course) {
+	assert(course);
+	free(course->name);
+	free(course);
 }
 
 // Returns the name of the course (shallow copy).
-const char *courseGetName(Course const * course) {
-	return NULL;
+const char *courseGetName(const Course* course) {
+	assert(course);
+	return course->name;
 }
 
 // Returns the grade of the course
-int courseGetGrade(Course const * course) {
-	return 0;
+int courseGetGrade(const Course* course) {
+	assert(course);
+	return course->grade;
 }
 
 // Creates a new course which is a deep copy of the course given in the parameter.
 // The caller of the function must free the return value when it is not needed anymore.
-Course *courseCopy(Course const * course) {
-	return NULL;
+Course* courseCopy(const Course* course) {
+	assert(course);
+	Course* dest = calloc(1, sizeof(Course));
+	dest->name = stringCopy(course->name);
+	dest->grade = course->grade;
+	return dest;
 }
--- transcript.c
+++ transcript.c
@@ -1,6 +1,15 @@
 #include "course.h"
 #include "transcript.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+struct Transcript_s {
+	char* name;
+	Course** courses;
+};
+
+char* stringCopy(const char* src);
 
 // This function creates a new transcript for student (the name is given in "studentName")
 // A Transcript contains the name of the owner of the transcript (whose transcript it is),
@@ -10,23 +19,54 @@
 // dynamic - you are not allowed to use arrays of static size.
 //
 // Initially the student has no completed courses.
-Transcript *tsConstruct(const char *studentName) {
-	return NULL;
+Transcript* tsConstruct(const char* studentName) {
+	assert(studentName);
+	Transcript* ts = calloc(1, sizeof(Transcript));
+	ts->name = stringCopy(studentName);
+	ts->courses = calloc(1, sizeof(Course*));
+	return ts;
+}
+
+void freeCourses(Course** courses) {
+	for (Course** c = courses; *c != NULL; c++) {
+		courseDestruct(*c);
+	}
+	free(courses);
+}
+
+int coursesCount(Course** courses) {
+	int count = 0;
+	while (*courses++) {
+		count++;
+	}
+	return count;
 }
 
 // This function frees all memory allocated by the abstract data type.
-void tsDestruct(Transcript * transcript) {
+void tsDestruct(Transcript* ts) {
+	assert(ts);
+	free(ts->name);
+	freeCourses(ts->courses);
+	free(ts);
 }
 
 // This function returns the name of the student (whose transcript this is)
 // The string is not copied, the function just returns the pointer.
-const char *tsGetName(Transcript const* transcript) {
-	return NULL;
+const char *tsGetName(Transcript const* ts) {
+	assert(ts);
+	return ts->name;
 }
 
 // This function returns the average grade of the completed courses.
-double tsAverageGrade(Transcript const *t) {
-	return 0.0;
+double tsAverageGrade(Transcript const* ts) {
+	assert(ts);
+	double sum = 0.0;
+	int count = 0;
+	for (int i = 0; ts->courses[i] != NULL; i++) {
+		sum += courseGetGrade(ts->courses[i]);
+		count++;
+	}
+	return sum / count;
 }
 
 // This function assigns a copy of the course array "courses" to the transcript.
@@ -36,7 +76,14 @@ double tsAverageGrade(Transcript const *t) {
 // "courses" is a pointer to the first element of an array of Course pointers.
 // The last element of the array is NULL. Elements of the array have been allocated using the
 // Course ADT.
-void tsSetCourseArray(Transcript *t, Course **courses) {
+void tsSetCourseArray(Transcript* ts, Course** courses) {
+	assert(ts);
+	assert(courses);
+	freeCourses(ts->courses);
+	ts->courses = calloc(coursesCount(courses) + 1, sizeof(Course*));
+	for (int i = 0; courses[i] != NULL; i++) {
+		ts->courses[i] = courseCopy(courses[i]);
+	}
 }
 
 // This function prints to the standard output all completed courses separated by newline.
@@ -44,10 +91,18 @@ void tsSetCourseArray(Transcript *t, Course **courses) {
 // <Course name>:<Grade>
 // For example:
 // AS-0.1101 Basic Course in C Programming:4
-void tsPrint(Transcript const * t) {
+void tsPrint(const Transcript* ts) {
+	assert(ts);
+	for (Course** courses = ts->courses; *courses != NULL; courses++) {
+		Course* course = *courses;
+		printf("%s:%d\n", courseGetName(course), courseGetGrade(course));
+	}
 }
 
 // Creates a deep copy of the transcript.
-Transcript *tsCopy(Transcript const * t) {
-	return NULL;
+Transcript* tsCopy(const Transcript* src) {
+	assert(src);
+	Transcript* dest = tsConstruct(src->name);
+	tsSetCourseArray(dest, src->courses);
+	return dest;
 }
