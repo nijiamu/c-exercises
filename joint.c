35c3dfd036eea31dda7d7112adb1bf9b49181c39
Ni Jiamu <nijiamu@hotmail.com>
2012-06-21 21:13:07 +0300

    Skeleton done

--- joint.c
+++ joint.c
@@ -1,32 +1,47 @@
 #include "joint.h"
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
+
+char* stringCopy(const char* src) {
+	assert(src);
+	char* dest = calloc(strlen(src) + 1, sizeof(char));
+	strcpy(dest, src);
+	return dest;
+}
 
 // initializes the joint with given name and sets parent to -1
 // name should be deep copied
 void initializeJoint(struct Joint* joint, const char* name){
-   memcpy(&joint->name, name, sizeof(joint->name)); 
-   (* joint).parent = -1;
+	assert(joint);
+	joint->name = stringCopy(name);
+	joint->parent = -1;
 }
 
 // Returns the name of the joint
 const char* getJointName(const struct Joint* joint){
-   char* jointName = (* joint).name;
-   return jointName;
+	assert(joint);
+	return joint->name;
 }
 
 // Sets the parent of joint
 // (This will be used in skeleton)
 void setJointParent(struct Joint* joint, size_t parent){
-   (* joint).parent = parent;
+	assert(joint);
+	joint->parent = parent;
 }
 
 // Return the parent of the joint
 size_t getJointParent(struct Joint* joint){
-  return (* joint).parent;
+	assert(joint);
+	return joint->parent;
 }
 
 // frees all memory allocated to the joint
 void destroyJoint(struct Joint* joint){
-   free(joint);
+	if (joint == NULL) {
+		return;
+	}
+	free(joint->name);
+	free(joint);
 }
--- skeleton.c
+++ skeleton.c
@@ -1,29 +1,60 @@
 #include "joint.h"
 #include "skeleton.h"
 #include <stddef.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+char* stringCopy2(const char* src) {
+	assert(src);
+	char* dest = calloc(strlen(src) + 1, sizeof(char));
+	strcpy(dest, src);
+	return dest;
+}
 
 // Creates new skeleton with given name and size
 // Name should be deep copied
 // Allocates space for given number of joints to jointArray
 // Fills the array with NULL values
 Skeleton createSkeleton(const char* name, size_t numJoints) {
+	assert(name);
 	Skeleton skeleton;
+	skeleton.name = stringCopy2(name);
+	skeleton.numJoints = numJoints;
+	skeleton.jointArray = calloc(numJoints, sizeof(struct Joint*));
 	return skeleton;
 }
-
 
 // Prints the name of the skeleton to the first row
 // Prints all the members of jointArray in individual lines in format (without the < and >):
 // <Joint name> <joint parent>
 // For empty elements in array prints "No joint attached"
 void printJoints(const Skeleton skeleton) {
+	printf("%s\n", skeleton.name);
+	for (size_t i = 0; i < skeleton.numJoints; i++) {
+		struct Joint* joint = skeleton.jointArray[i];
+		if (joint != NULL) {
+			printf("%s %d\n", joint->name, (int) joint->parent);
+		} else {
+			printf("No joint attached\n");
+		}
+	}
 }
 
 // Adds the joint to the given index of skeleton
 // Changes te parent of the joint to given value
 void addJoint(const Skeleton skeleton, size_t index, struct Joint* joint, size_t parent) {
+	assert(joint);
+	skeleton.jointArray[index] = joint;
+	joint->parent = parent;
 }
 
 // frees all memory allocated to the skeleton struct
 void destroySkeleton(Skeleton skeleton) {
+	free(skeleton.name);
+	for (size_t i = 0; i < skeleton.numJoints; i++) {
+		destroyJoint(skeleton.jointArray[i]);
+	}
+	free(skeleton.jointArray);
 }
